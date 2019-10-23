c9d708b19817d1bbd88436a81b4cd4b40cc87ed6
luanBrandao <luanbrandao4@gmail.com>
2019-10-23 12:54:12 -0300

    Conceitos do React

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// yarn add @babel/core @babel/preset-env @babel/preset-react webpack webpack-cli
+// yarn add react react-dom 
+// yarn add babel-loader -D
+// yarn add webpack-dev-server -D
+const soma = (a,b) => a+ b;
+
+alert(soma(1,4));
